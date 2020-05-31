30c473af269600a709d52de3439b2d00a3eadbc9
johnmartins <johnmartins1992@gmail.com>
2020-06-01 00:30:57 +0200

    textfields in cells

--- static/js/morph-matrix/matrix.js
+++ static/js/morph-matrix/matrix.js
@@ -1,6 +1,9 @@
 'use strict'
 
 class MorphMatrix {
+    // Parameters
+    cellWidth = "100px"
+
     // Important layout vars
     containerID = null
     containerElement = null
@@ -23,28 +26,46 @@ class MorphMatrix {
         this.tableElement.appendChild(this.tbodyElement)
         this.containerElement.appendChild(this.tableElement)
 
-        this.setupTableControls()
+        this._setupTableControls()
     }
 
-    setupTableControls () {
+    _setupTableControls () {
         let firstRow = this.tbodyElement.insertRow()
         let rootCell = firstRow.insertCell()
         rootCell.classList.add('mm-label-cell')
         rootCell.innerHTML = "Functional Requirements"
+        rootCell.width = this.cellWidth
 
         this.dsLabelCell = firstRow.insertCell()
         this.dsLabelCell.innerHTML = "Design solutions"
         this.dsLabelCell.align = "center"
         this.dsLabelCell.classList.add('mm-label-cell')
         this.dsLabelCell.colSpan = 1
+        this.dsLabelCell.width = this.cellWidth
 
         let secondRow = this.tbodyElement.insertRow()
         let addRowCell = secondRow.insertCell()
         addRowCell.innerHTML = "Add FR"
         addRowCell.classList.add('mm-add-cell')
+        addRowCell.width = this.cellWidth
         addRowCell.onclick = () => {
             this.addFunctionalRequirement ()
         }
+    }
+
+    _createCellForm (cellElement, placeholder) {
+        let cellForm = document.createElement('textarea')
+        cellForm.placeholder = placeholder
+        cellForm.classList.add('cellForm')
+        cellForm.rows = 2
+        cellForm.maxLength = 40
+        cellForm.wrap = "soft"
+        cellForm.onkeypress = (evt) => {
+            if (evt.keyCode === 13) return false
+        }
+
+        cellElement.appendChild(cellForm)
+        cellForm.focus()
     }
 
     addFunctionalRequirement () {
@@ -52,16 +73,17 @@ class MorphMatrix {
         let position = this.tableElement.rows.length - 1
         let newRow = this.tbodyElement.insertRow(position)
         let newCell = newRow.insertCell()
-        newCell.innerHTML = `FR ${position}`
+        newCell.width = this.cellWidth
+        
+        this._createCellForm(newCell, `Functional Requirement ${position}`)
 
         let newAddCell = newRow.insertCell()
         newAddCell.innerHTML = 'Add DS'
+        newAddCell.width = this.cellWidth
         newAddCell.classList.add('mm-add-cell')
         newAddCell.onclick = () => {
             this.addDesignSolution(position)
         }
-
-        console.log("ADD FR")
     }
 
     addDesignSolution (position) {
@@ -69,14 +91,17 @@ class MorphMatrix {
         let cellPosition = row.cells.length - 1
 
         let newCell = row.insertCell(cellPosition)
-        newCell.innerHTML = `DS ${cellPosition}`
+        newCell.width = this.cellWidth
+
+        this._createCellForm(newCell, `Design Solution ${position}`)
         
         if (this.dsLabelCell.colSpan < row.cells.length - 2) {
             this.dsLabelCell.colSpan = row.cells.length - 2
         }
-        
-        console.log("ADD DS")
     }
+
+
 }
+
 
 module.exports = MorphMatrix

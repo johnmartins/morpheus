1e2fe332731dcd2ca1dda440c3b0e8a9d106f53f
johnmartins <johnmartins1992@gmail.com>
2020-06-01 09:36:28 +0200

    Various UI improvements to matrix

--- static/js/morph-matrix/matrix.js
+++ static/js/morph-matrix/matrix.js
@@ -2,7 +2,7 @@
 
 class MorphMatrix {
     // Parameters
-    cellWidth = "100px"
+    cellWidth = "150px"
 
     // Important layout vars
     containerID = null
@@ -45,7 +45,9 @@ class MorphMatrix {
 
         let secondRow = this.tbodyElement.insertRow()
         let addRowCell = secondRow.insertCell()
-        addRowCell.innerHTML = "Add FR"
+        addRowCell.innerHTML = '<i style="font-weight: bold;" class="far fa-plus-square"></i> FR'
+        addRowCell.align = "center"
+        addRowCell.style.fontSize = "1rem"
         addRowCell.classList.add('mm-add-cell')
         addRowCell.width = this.cellWidth
         addRowCell.onclick = () => {
@@ -53,10 +55,11 @@ class MorphMatrix {
         }
     }
 
-    _createCellForm (cellElement, placeholder) {
+    _createCellForm (cellElement, placeholder, styleClass) {
         let cellForm = document.createElement('textarea')
         cellForm.placeholder = placeholder
-        cellForm.classList.add('cellForm')
+        cellForm.classList.add('cell-form')
+        if (styleClass) cellForm.classList.add(styleClass)
         cellForm.rows = 2
         cellForm.maxLength = 40
         cellForm.wrap = "soft"
@@ -75,10 +78,12 @@ class MorphMatrix {
         let newCell = newRow.insertCell()
         newCell.width = this.cellWidth
         
-        this._createCellForm(newCell, `Functional Requirement ${position}`)
+        this._createCellForm(newCell, `Functional Requirement ${position}`, 'func-req')
 
         let newAddCell = newRow.insertCell()
-        newAddCell.innerHTML = 'Add DS'
+        newAddCell.innerHTML = '<i style="font-weight: bold;" class="far fa-plus-square"></i> DS'
+        newAddCell.style.fontSize = "1rem"
+        newAddCell.align = "center"
         newAddCell.width = this.cellWidth
         newAddCell.classList.add('mm-add-cell')
         newAddCell.onclick = () => {

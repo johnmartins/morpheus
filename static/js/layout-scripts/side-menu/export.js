c5cf54b42e7ce99b6cecd0f4a97f9b8ed7d62d4a
johnmartins <johnmartins1992@gmail.com>
2020-06-23 08:52:49 +0200

    Drop down menu for selecting solutions

--- static/js/layout-scripts/side-menu/export.js
+++ static/js/layout-scripts/side-menu/export.js
@@ -1,8 +1,49 @@
 'use strict'
 
+const NAME_SOLUTION_EXPORT_TARGET = 'export-solutions-radio'
+
 module.exports = {
 
     setupListeners: () => {
-        
+        let tabExport = document.getElementById('tab-export')
+        let btnExportMatrix = document.getElementById('btn-export-matrix')
+        let btnExportSolutions = document.getElementById('btn-export-solutions')
+        let solutionSelector = document.getElementById('export-solution-selector')
+
+        // Listen for radio input changes
+        tabExport.onclick = (evt) => {
+            const name = evt.target.name
+            if (!name) return
+            if (name === NAME_SOLUTION_EXPORT_TARGET) {
+                let val = tabExport.querySelector(`[name="${NAME_SOLUTION_EXPORT_TARGET}"]:checked`).value
+
+                if (val === 'specific') {
+                    // Show selector
+                    console.log("show")
+                } else {
+                    // Hide selector
+                    console.log("hide")
+                }
+            }
+        }
+
+        btnExportMatrix.onclick = () => {
+
+        }
+
+        btnExportSolutions.onclick = () => {
+
+        }
+
+        // Solution selector
+        solutionSelector.onclick = () => {
+            let open = solutionSelector.classList.contains('open')
+            if (open) {
+                solutionSelector.classList.remove('open')
+            } else {
+                solutionSelector.classList.add('open')
+            }
+        }
     }
 }
+

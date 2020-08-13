ec755a02a56e23bbab355090e1fbdf2bfeb65ab0
johnmartins <johnmartins1992@gmail.com>
2020-08-13 18:57:30 +0200

    Moved config out and made space for cert

---
+++ forge.config.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const fs = require('fs')
+const path = require('path')
+
+const skipCertificate = true
+const certFilePath = path.join('./', 'build-resources', 'certificates', 'win.pfx')
+const passwordFilePath = path.join('./', 'build-resources', 'certificates', 'win-pwd.txt')
+
+module.exports = {
+    packagerConfig: {
+        icon: "build-resources/icons/app-icon"
+    },
+    makers: [
+        {
+            name: "@electron-forge/maker-squirrel",
+            config: {
+                certificateFile: skipCertificate ? "" : certFilePath,
+                certificatePassword: skipCertificate ? "" : getPassword()
+            }
+        }
+    ]
+}
+
+function getPassword () {
+    // Use FS to sync read passwordFilePath and return the contents without linebreaks
+    return "abcdefg"
+}

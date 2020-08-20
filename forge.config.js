4e33a67bfd7b096b9f70afd4b276657838ad98bf
johnmartins <johnmartins1992@gmail.com>
2020-08-20 09:50:39 +0200

    Added file exists check to sign process

--- forge.config.js
+++ forge.config.js
@@ -2,10 +2,27 @@
 
 const fs = require('fs')
 const path = require('path')
+const { exit } = require('process')
 
-const skipCertificate = true
+const skipCertificate = false
 const certFilePath = path.join('./', 'build-resources', 'certificates', 'win.pfx')
 const passwordFilePath = path.join('./', 'build-resources', 'certificates', 'win-pwd.txt')
+
+if (!skipCertificate) {
+    console.log('Skip certificate: false. Attempting to sign code.')
+    try {
+        let certExists = fs.existsSync(certFilePath)
+        let pwdExists = fs.existsSync(passwordFilePath)
+
+        if (!certExists || !pwdExists) {
+            throw new Error('Required files (win.pfx and win-pwd.txt) does not exist in directory "./build-resources/certificates"')
+        }
+    } catch (e) {
+        console.error('Failed to sign code. Message: '+e.message)
+        exit(1)
+    }
+
+}
 
 module.exports = {
     packagerConfig: {
@@ -25,5 +42,7 @@ module.exports = {
 
 function getPassword () {
     // Use FS to sync read passwordFilePath and return the contents without linebreaks
-    return "abcdefg"
+    let content = fs.readFileSync(passwordFilePath, {encoding: 'utf8'})
+    console.log('pwd: '+content.replace('\n','').replace('\r\n',''))
+    return content.replace('\n','').replace('\r\n','')
 }

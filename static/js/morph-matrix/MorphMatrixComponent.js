5a470b9768c9d4dfb4037ff0893812cc6be44ae4
johnmartins <johnmartins1992@gmail.com>
2021-02-03 09:21:45 +0100

    started comstructing matrix component

---
+++ static/js/morph-matrix/MorphMatrixComponent.js
@@ -0,0 +1,20 @@
+'use strict'
+
+class MorphMatrixComponent {
+
+    // Important layout vars
+    containerID = null
+    containerElement = null
+    titleContainerElement = null
+    titleElement = null
+    tableElement = null
+    tbodyElement = null
+    dsLabelCell = null
+    canvasOverlay = null
+
+    constructor () {
+
+    }
+
+
+}
